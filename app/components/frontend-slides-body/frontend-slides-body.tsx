0dd6e231ff06d24d94d48a0abda05737c9193ce8
dkanukov <dkanukov@ozon.ru>
2023-10-26 13:55:43 +0300

    text

--- app/components/frontend-slides-body/frontend-slides-body.tsx
+++ app/components/frontend-slides-body/frontend-slides-body.tsx
@@ -3,10 +3,14 @@ import Carousel from 'react-multi-carousel'
 import Image from 'next/image'
 import 'react-multi-carousel/lib/styles.css'
 import './frontend-slides-body.css'
-import { Card, CardContent } from '@mui/joy'
+import {AspectRatio, Card, CardContent } from '@mui/joy'
 import React from 'react'
 
 import Gevorg from '@/public/gevorg.jpeg'
+import Png1 from '@/public/1.png'
+import Png2 from '@/public/2.png'
+import Png3 from '@/public/3.png'
+import Png4 from '@/public/4.png'
 const FrontendSlidesBody = () => {
 	return (
 		<div>
@@ -68,24 +72,48 @@ const FrontendSlidesBody = () => {
 			>
 				{/*TODO change photos to map-examples*/}
 				<Card>
-					<CardContent>
-						<Image src={Gevorg} alt="" />
-					</CardContent>
+					<AspectRatio
+						sx={{ minWidth: 200 }}
+						flex
+						ratio="1"
+					>
+						<CardContent>
+							<Image src={Png1} alt="" />
+						</CardContent>
+					</AspectRatio>
 				</Card>
 				<Card>
-					<CardContent>
-						<Image src={Gevorg} alt="" />
-					</CardContent>
+					<AspectRatio
+						sx={{ minWidth: 200 }}
+						flex
+						ratio="1"
+					>
+						<CardContent>
+							<Image src={Png2} alt="" />
+						</CardContent>
+					</AspectRatio>
 				</Card>
 				<Card>
-					<CardContent>
-						<Image src={Gevorg} alt="" />
-					</CardContent>
+					<AspectRatio
+						sx={{ minWidth: 200 }}
+						flex
+						ratio="1"
+					>
+						<CardContent>
+							<Image src={Png3} alt="" />
+						</CardContent>
+					</AspectRatio>
 				</Card>
 				<Card>
-					<CardContent>
-						<Image src={Gevorg} alt="" />
-					</CardContent>
+					<AspectRatio
+						sx={{ minWidth: 200 }}
+						flex
+						ratio="1"
+					>
+						<CardContent>
+							<Image src={Png4} alt="" />
+						</CardContent>
+					</AspectRatio>
 				</Card>
 			</Carousel>
 		</div>
