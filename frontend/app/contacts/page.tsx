9701149f405f1d7804f4b190269e3b0a861fe34f
Denis Kanukov <dkanukov@ozon.ru>
2024-02-03 15:28:54 +0300

    upd ui

--- frontend/app/contacts/page.tsx
+++ frontend/app/contacts/page.tsx
@@ -1,267 +1,81 @@
 'use client'
 import { useState } from 'react'
-import { AspectRatio, Box, Card, CardContent, Chip, Grid, IconButton, Link, Typography } from '@mui/joy'
-import { ThumbDown, ThumbUp } from '@mui/icons-material'
+import { Card, Col, Flex, Image, Row, Typography } from 'antd'
 
 import styles from './contacts.module.css'
 
+const { Grid } = Card
+const { Text, Link } = Typography
+
 export default function Contacts() {
-	const [gevorgLikes, setGevorgLikes] = useState(0)
-	const [gevorgDislikes, setGevorgDislikes] = useState(100)
-	const [denisLikes, setDenisLikes] = useState(100)
-	const [denisDislikes, setDenisDislikes] = useState(-42)
-
-	const handleGevorgLike = () => setGevorgLikes(gevorgLikes + 1)
-
-	const handleGevorgDislike = () => setGevorgDislikes(gevorgDislikes + 1)
-
-	const handleDenisLike = () => setDenisLikes(denisLikes + 1)
-
 	return (
-		<div>
-			<Grid
-				spacing={2}
-				container
+		<div className={styles.root}>
+			<Row
+				gutter={12}
 			>
-				<Grid
-					xs={12}
-					sm={12}
-					md={6}
-					lg={6}
+				<Col
+					span={12}
 				>
-					<Card
-						orientation="horizontal"
-					>
-						<AspectRatio
-							sx={{ minWidth: 200 }}
-							flex
-							ratio="1"
-						>
-							<img
-								src="/gevorg.jpeg"
-								alt=""
+					<Card title={'Цатурян Геворг - backend разработчик'}>
+						<Grid >
+							<Image
+								alt="Геворг"
+								src={'/gevorg.jpeg'}
 							/>
-						</AspectRatio>
-						<CardContent>
-							<Typography
-								level="h2"
-							>
-							Цатурян Геворг
-							</Typography>
-							<Typography
-								level="body-sm"
-							>
-							Студент 4ого курса ИВТ
-							</Typography>
-							<Typography
-								sx={{
-									marginTop: '10px',
-								}}
-								level="body-lg"
-							>
-							Номер телефона:	<Link href="tel: +7(926)-679-01-45"> +7(926)-679-01-45 </Link>
-							</Typography>
-							<Typography
-								level="body-lg"
-							>
-							email: <Link href="mailto:gev.tsat@gmail.com">gev.tsat@gmail.com</Link>
-							</Typography>
-
-							<div className={styles.likes}>
-								<IconButton
-									sx={{
-										display: 'flex',
-										gap: '8px',
-									}}
-									onClick={handleGevorgLike}
-								>
-									{gevorgLikes}
-									<ThumbUp
-										sx={{
-											margintop: '-5px',
-										}}
-									/>
-								</IconButton>
-								<IconButton
-									sx={{
-										display: 'flex',
-										gap: '8px',
-									}}
-									onClick={handleGevorgDislike}
-								>
-									{gevorgDislikes}
-									<ThumbDown
-										sx={{
-											margintop: '-5px',
-										}}
-									/>
-								</IconButton>
-							</div>
-						</CardContent>
-					</Card>
-				</Grid>
-				<Grid
-					xs={12}
-					sm={12}
-					md={6}
-					lg={6}
-				>
-					<Card
-						orientation="horizontal"
-					>
-						<AspectRatio
-							sx={{ minWidth: 200 }}
-							flex
-							ratio="1"
+						</Grid>
+						<Flex
+							vertical
+							gap={12}
+							style={{
+								paddingTop: '20px',
+								paddingLeft: '20px',
+							}}
 						>
-							<img
-								src="/denis.jpeg"
-								alt=""
-							/>
-						</AspectRatio>
-						<CardContent>
-							<Typography
-								level="h2"
-							>
-								Кануков Денис
-							</Typography>
-							<Typography
-								level="body-sm"
-							>
-							Студент 4ого курса ИВТ
-							</Typography>
-							<Typography
-								sx={{
-									marginTop: '10px',
-								}}
-								level="body-lg"
-							>
-							Номер телефона: <Link href="tel:89857282003">+7(985)728-20-03</Link>
-							</Typography>
-							<Typography
-								level="body-lg"
-							>
-							email: <Link href="mailto:kanu.denis@gmail.com">kanu.denis@gmail.com</Link>
-							</Typography>
-
-							<div className={styles.likes}>
-								<IconButton
-									sx={{
-										display: 'flex',
-										gap: '8px',
-									}}
-									onClick={handleDenisLike}
-								>
-									{denisLikes}
-									<ThumbUp
-										sx={{
-											margintop: '-5px',
-										}}
-									/>
-								</IconButton>
-								<IconButton
-									disabled
-									sx={{
-										display: 'flex',
-										gap: '8px',
-									}}
-								>
-									{denisDislikes}
-									<ThumbDown
-										sx={{
-											margintop: '-5px',
-										}}
-									/>
-								</IconButton>
-							</div>
-						</CardContent>
+							<Text strong>Студент 4 курса ИВТ</Text>
+							<Text>email: <Link href="mailto:gev.tsat@gmail.com">gev.tsat@gmail.com</Link></Text>
+						</Flex>
 					</Card>
-				</Grid>
-				<Grid
-					xs={12}
-					sm={12}
-					md={12}
-					lg={12}
+				</Col>
+				<Col
+					span={12}
 				>
-					<Card
-						orientation="horizontal"
-					>
-						<AspectRatio
-							sx={{ minWidth: 200 }}
-							flex
-							ratio="1"
+					<Card title={'Кануков Денис - frontend разработчик'}>
+						<Grid>
+							<Image
+								alt="Денис"
+								src={'/denis.jpeg'}
+							/>
+						</Grid>
+						<Flex
+							vertical
+							gap={12}
+							style={{
+								paddingTop: '20px',
+								paddingLeft: '20px',
+							}}
 						>
-							<img
-								src="/nadejda.jpeg"
-								alt=""
-							/>
-						</AspectRatio>
-						<CardContent>
-							<Typography
-								level="h2"
-							>
-								Трубочкина Надежда Константиновна
-							</Typography>
-							<Typography
-								level="body-sm"
-							>
-								Руководитель
-							</Typography>
-							<div className={styles.contacts}>
-								<Typography
-									sx={{
-										marginTop: '10px',
-										display: 'flex',
-										gap: '5px',
-									}}
-									level="body-lg"
-								>
-
-								</Typography>								Номер телефона:
-								<Chip
-									size="sm"
-									variant="solid"
-									color="danger"
-								>
-									TOP SECRET
-								</Chip>
-							</div>
-
-							<div className={styles.contacts}>
-								<Typography
-									sx={{
-										display: 'flex',
-										gap: '5px',
-									}}
-									level="body-lg"
-								>
-
-								</Typography>
-							email:
-								<Chip
-									size="sm"
-									variant="solid"
-									color="danger"
-								>
-									TOP SECRET
-								</Chip>
-
-							</div>
-						</CardContent>
+							<Text strong>Студент 4 курса ИВТ</Text>
+							<Text>email: <Link href="mailto:gev.tsat@gmail.com">gev.tsat@gmail.com</Link></Text>
+						</Flex>
 					</Card>
-				</Grid>
-			</Grid>
-			<Box
-				sx={{
-					marginTop: '10px',
-				}}
-			>
-				<iframe
-					src="https://yandex.ru/map-widget/v1/?um=constructor%3A74af39facb337d144cfd686260e59d621e1450d8335b89c25ed0a17cd536d2cd&amp;source=constructor" width={'100%'}
-					height="452" frameBorder="0"
-				>
-				</iframe>
-			</Box>
+				</Col>
+			</Row>
+			<Row>
+				<Col span={24}>
+					<Card title={'Трубочкина Надежда Константиновна - руководитель'}>
+						<Image
+							alt="Надежда"
+							src={'/nadejda.jpeg'}
+						/>
+					</Card>
+				</Col>
+			</Row>
 		</div>
 	)
 }
+
+// <iframe
+// 					src="https://yandex.ru/map-widget/v1/?um=constructor%3A74af39facb337d144cfd686260e59d621e1450d8335b89c25ed0a17cd536d2cd&amp;source=constructor" width={'100%'}
+// 					height="452" frameBorder="0"
+// 				>
+// 				</iframe>
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,17 +1,91 @@
-import { Box, Container } from '@mui/material'
-import React from 'react'
+'use client'
+import { useEffect, useRef } from 'react'
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { Typography } from 'antd'
 
-import LoglineBody from './components/logline-body/logline-body'
-import FunctionalityBody from './components/functionality-body/functionality-body'
+import styles from './index.module.css'
+
+const { Title } = Typography
 
 export default function Landing() {
+	const containerRef = useRef<HTMLDivElement>(null)
+	const renderer = useRef(new THREE.WebGLRenderer())
+
+	const setupScene = () => {
+		const scene = new THREE.Scene()
+		const ambientLight = new THREE.AmbientLight()
+		const light = new THREE.PointLight(0xffffff, 1)
+
+		light.position.set(0.8, 1.4, 1.0)
+
+		scene.add(light)
+		scene.add(ambientLight)
+
+		return scene
+	}
+
+	const setupCamera = () => {
+		const camera = new THREE.PerspectiveCamera(75)
+
+		camera.position.set(0.8, 1.4, 1.0)
+		camera.translateZ(20)
+
+		return camera
+	}
+
+	const setupControls = (camera: THREE.Camera) => {
+		const controls = new OrbitControls(camera, renderer.current.domElement)
+
+		controls.enableRotate = true
+
+		return controls
+	}
+
+	useEffect(() => {
+		const loader = new GLTFLoader()
+		const scene = setupScene()
+		const camera = setupCamera()
+		const controls = setupControls(camera)
+
+		let objectCamera: THREE.Group<THREE.Object3DEventMap> | null = null
+
+		renderer.current.setSize(300, 300)
+
+		loader.load('/scene.gltf', (obj) => {
+			obj.scene.position.set(0, -5, 0)
+			scene.add(obj.scene)
+			objectCamera = obj.scene
+		}, undefined, (error) => {console.log(error)})
+
+		const render = () => {
+			if (objectCamera) {
+				objectCamera.rotateY(0.006)
+			}
+
+			const canvas = renderer.current.domElement
+			camera.aspect = canvas.clientWidth / canvas.clientHeight
+			camera.updateProjectionMatrix()
+
+			controls.update()
+			renderer.current.render(scene, camera)
+
+			if (typeof requestAnimationFrame !== 'undefined') {
+				requestAnimationFrame(render)
+			}
+		}
+
+		containerRef.current?.appendChild(renderer.current.domElement)
+		render()
+	})
+
 	return (
-		<Box>
-			<h1 className="heading">
-				Следилка это who?
-			</h1>
-			<LoglineBody />
-			<FunctionalityBody />
-		</Box>
+		<div className={styles.root}>
+			<div className={styles.sceneContainer} ref={containerRef}/>
+			<Title className={styles.headerText}>
+					Sledilka
+			</Title>
+		</div>
 	)
 }
