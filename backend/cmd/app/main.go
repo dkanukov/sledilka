21cc566ec589f2d18bb7bf81baae2d25216813cb
gtsaturyan <gtsaturyan@ozon.ru>
2024-05-23 21:02:30 +0300

    networking service for linux without mocks

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -1,28 +1,17 @@
 package main
 
 import (
-	"backend/internal/conv"
-	"backend/internal/dbmodel"
-	mock_network "backend/internal/mocks/mock_networking"
-	"backend/internal/network"
-	"backend/internal/utils"
-	"context"
-	"database/sql"
-	"errors"
-	"fmt"
-	"github.com/golang/mock/gomock"
-	"log"
-	"math/rand"
-	"net"
-	"time"
-
 	_ "backend/docs"
 	"backend/internal/app/sledilka"
 	"backend/internal/db"
+	"backend/internal/network"
 	"backend/internal/tokener"
+	"context"
+	"fmt"
 	"github.com/compose-spec/compose-go/v2/cli"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"log"
 )
 
 //	@title			Sledilka API
@@ -88,8 +77,8 @@ func main() {
 
 	conn, err := grpc.Dial(
 		fmt.Sprintf("%s:%v",
-			//"0.0.0.0",
-			tokenerInfo.Name,
+			"0.0.0.0",
+			//tokenerInfo.Name,
 			tokenerInfo.Ports[0].Published),
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 	)
@@ -98,114 +87,117 @@ func main() {
 	}
 	tokenerClient := tokener.NewTokenerClient(conn)
 
-	//conn, err = grpc.Dial(
-	//	fmt.Sprintf("%s:%v", "0.0.0.0", 8282),
-	//	grpc.WithTransportCredentials(insecure.NewCredentials()),
-	//)
-	//
-	//networkerClient := network.NewNetworkClient(conn)
-	ctrl := gomock.NewController(MockLogger{})
-	subscribeClient := mock_network.NewMockNetwork_SubscribeClient(ctrl)
-	subscribeClient.EXPECT().Recv().DoAndReturn(
-		func() (*network.DeviceStatus, error) {
-			time.Sleep(10 * time.Second)
-			log.Println("taking random device from db")
-			dev, err := dbmodel.New(DBConnection).GetRandomDevice(ctx)
-			if err != nil {
-				return nil, sql.ErrNoRows
-			}
-			return &network.DeviceStatus{
-				MacAddress: dev.MacAddress,
-				IsActive:   utils.RandBool(),
-				IpAddress:  dev.IpAddress,
-			}, nil
-		},
-	).AnyTimes()
-	networkerClient := mock_network.NewMockNetworkClient(ctrl)
-	networkerClient.EXPECT().Subscribe(gomock.Any(), gomock.Any()).Return(subscribeClient, nil).AnyTimes()
-	networkerClient.EXPECT().GetAllMacAddresses(gomock.Any(), gomock.Any()).DoAndReturn(
-		func(ctx context.Context, in *network.Empty, opts ...grpc.CallOption) (*network.GetAllMacAddressesResponse, error) {
-			macs, err := dbmodel.New(DBConnection).GetAllDevicesMacs(ctx)
-			if err != nil {
-				return nil, errors.New("db error")
-			}
-			resp := &network.GetAllMacAddressesResponse{
-				Devices: make([]*network.DeviceStatus, 0, len(macs)),
-			}
-			for _, mac := range macs {
-				resp.Devices = append(resp.Devices, &network.DeviceStatus{
-					MacAddress: mac,
-					IsActive:   utils.RandBool(),
-					IpAddress:  conv.StringToStringp("127.0.0.1"),
-				})
-			}
-			resp.Devices = append(resp.Devices, &network.DeviceStatus{
-				MacAddress: GenerateMac().String(),
-				IsActive:   true,
-				IpAddress:  conv.StringToStringp("127.0.0.1"),
-			})
-			resp.Devices = append(resp.Devices, &network.DeviceStatus{
-				MacAddress: GenerateMac().String(),
-				IsActive:   true,
-				IpAddress:  conv.StringToStringp("127.0.0.1"),
-			})
-			resp.Devices = append(resp.Devices, &network.DeviceStatus{
-				MacAddress: GenerateMac().String(),
-				IsActive:   true,
-				IpAddress:  conv.StringToStringp("127.0.0.1"),
-			})
-			return resp, nil
-		},
-	).AnyTimes()
+	conn, err = grpc.Dial(
+		fmt.Sprintf("%s:%v", "0.0.0.0", 8282),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
 
-	networkerClient.EXPECT().IsActiveListDevice(gomock.Any(), gomock.Any()).DoAndReturn(
-		func(_ context.Context, req *network.IsActiveListDeviceRequest, _ ...grpc.CallOption) (*network.IsActiveListDeviceResponse, error) {
-			resp := &network.IsActiveListDeviceResponse{
-				Devices: make([]*network.DeviceStatus, 0, len(req.GetDevices())),
-			}
-			for _, dev := range req.GetDevices() {
-				resp.Devices = append(resp.Devices, &network.DeviceStatus{
-					MacAddress: dev.GetMacAddress(),
-					IsActive:   utils.RandBool(),
-					IpAddress:  conv.StringToStringp("127.0.0.1"),
-				})
-			}
-			return resp, nil
-		},
-	).AnyTimes()
+	networkerClient := network.NewNetworkClient(conn)
+	//ctrl := gomock.NewController(MockLogger{})
+	//subscribeClient := mock_network.NewMockNetwork_SubscribeClient(ctrl)
+	//subscribeClient.EXPECT().Recv().DoAndReturn(
+	//	func() (*network.DeviceStatus, error) {
+	//		time.Sleep(10 * time.Second)
+	//		log.Println("taking random device from db")
+	//		dev, err := dbmodel.New(DBConnection).GetRandomDevice(ctx)
+	//		if err != nil {
+	//			return nil, sql.ErrNoRows
+	//		}
+	//		return &network.DeviceStatus{
+	//			MacAddress: dev.MacAddress,
+	//			IsActive:   utils.RandBool(),
+	//			IpAddress:  dev.IpAddress,
+	//		}, nil
+	//	},
+	//).AnyTimes()
+	//networkerClient := mock_network.NewMockNetworkClient(ctrl)
+	//networkerClient.EXPECT().Subscribe(gomock.Any(), gomock.Any()).Return(subscribeClient, nil).AnyTimes()
+	//networkerClient.EXPECT().GetAllMacAddresses(gomock.Any(), gomock.Any()).DoAndReturn(
+	//	func(ctx context.Context, in *network.Empty, opts ...grpc.CallOption) (*network.GetAllMacAddressesResponse, error) {
+	//		macs, err := dbmodel.New(DBConnection).GetAllDevicesMacs(ctx)
+	//		if err != nil {
+	//			return nil, errors.New("db error")
+	//		}
+	//		resp := &network.GetAllMacAddressesResponse{
+	//			Devices: make([]*network.DeviceStatus, 0, len(macs)),
+	//		}
+	//		for _, mac := range macs {
+	//			resp.Devices = append(resp.Devices, &network.DeviceStatus{
+	//				MacAddress: mac,
+	//				IsActive:   utils.RandBool(),
+	//				IpAddress:  conv.StringToStringp("127.0.0.1"),
+	//			})
+	//		}
+	//		resp.Devices = append(resp.Devices, &network.DeviceStatus{
+	//			MacAddress: GenerateMac().String(),
+	//			IsActive:   true,
+	//			IpAddress:  conv.StringToStringp("127.0.0.1"),
+	//		})
+	//		resp.Devices = append(resp.Devices, &network.DeviceStatus{
+	//			MacAddress: GenerateMac().String(),
+	//			IsActive:   true,
+	//			IpAddress:  conv.StringToStringp("127.0.0.1"),
+	//		})
+	//		resp.Devices = append(resp.Devices, &network.DeviceStatus{
+	//			MacAddress: GenerateMac().String(),
+	//			IsActive:   true,
+	//			IpAddress:  conv.StringToStringp("127.0.0.1"),
+	//		})
+	//		return resp, nil
+	//	},
+	//).AnyTimes()
+	//
+	//networkerClient.EXPECT().IsActiveListDevice(gomock.Any(), gomock.Any()).DoAndReturn(
+	//	func(_ context.Context, req *network.IsActiveListDeviceRequest, _ ...grpc.CallOption) (*network.IsActiveListDeviceResponse, error) {
+	//		resp := &network.IsActiveListDeviceResponse{
+	//			Devices: make([]*network.DeviceStatus, 0, len(req.GetDevices())),
+	//		}
+	//		for _, dev := range req.GetDevices() {
+	//			resp.Devices = append(resp.Devices, &network.DeviceStatus{
+	//				MacAddress: dev.GetMacAddress(),
+	//				IsActive:   utils.RandBool(),
+	//				IpAddress:  conv.StringToStringp("127.0.0.1"),
+	//			})
+	//		}
+	//		return resp, nil
+	//	},
+	//).AnyTimes()
 
 	app := sledilka.New(
 		DBConnection,
 		tokenerClient,
 		networkerClient,
 		//fmt.Sprintf("http://0.0.0.0:%v/", 8181),
-		fmt.Sprintf("http://%v:%v/", streamingInfo.Name, streamingInfo.Ports[0].Published),
+		fmt.Sprintf("http://%v:%v/",
+			//streamingInfo.Name,
+			"0.0.0.0",
+			streamingInfo.Ports[0].Published),
 		"0.0.0.0:8081")
 	log.Fatal(app.Run())
 }
 
-type MockLogger struct{}
-
-func (m MockLogger) Errorf(format string, args ...interface{}) {
-	log.Printf(format, args...)
-}
-
-func (m MockLogger) Fatalf(format string, args ...interface{}) {
-	log.Fatalf(format, args...)
-}
-
-func GenerateMac() net.HardwareAddr {
-	buf := make([]byte, 6)
-	var mac net.HardwareAddr
-
-	_, err := rand.Read(buf)
-	if err != nil {
-	}
-
-	// Set the local bit
-	buf[0] |= 2
-
-	mac = append(mac, buf[0], buf[1], buf[2], buf[3], buf[4], buf[5])
-
-	return mac
-}
+//type MockLogger struct{}
+//
+//func (m MockLogger) Errorf(format string, args ...interface{}) {
+//	log.Printf(format, args...)
+//}
+//
+//func (m MockLogger) Fatalf(format string, args ...interface{}) {
+//	log.Fatalf(format, args...)
+//}
+//
+//func GenerateMac() net.HardwareAddr {
+//	buf := make([]byte, 6)
+//	var mac net.HardwareAddr
+//
+//	_, err := rand.Read(buf)
+//	if err != nil {
+//	}
+//
+//	// Set the local bit
+//	buf[0] |= 2
+//
+//	mac = append(mac, buf[0], buf[1], buf[2], buf[3], buf[4], buf[5])
+//
+//	return mac
+//}
--- networking-service/go.mod
+++ networking-service/go.mod
@@ -10,25 +10,10 @@ require (
 )
 
 require (
-	github.com/compose-spec/compose-go/v2 v2.1.0 // indirect
-	github.com/distribution/reference v0.5.0 // indirect
-	github.com/docker/go-connections v0.4.0 // indirect
-	github.com/docker/go-units v0.5.0 // indirect
-	github.com/mattn/go-shellwords v1.0.12 // indirect
 	github.com/mdlayher/arp v0.0.0-20191213142603-f72070a231fc // indirect
 	github.com/mdlayher/ethernet v0.0.0-20190313224307-5b5fc417d966 // indirect
 	github.com/mdlayher/raw v0.0.0-20190313224157-43dbcdd7739d // indirect
-	github.com/mitchellh/copystructure v1.2.0 // indirect
-	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/mitchellh/reflectwalk v1.0.2 // indirect
-	github.com/opencontainers/go-digest v1.0.0 // indirect
-	github.com/sirupsen/logrus v1.9.0 // indirect
-	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
-	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
-	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
-	golang.org/x/exp v0.0.0-20240112132812-db7319d0e0e3 // indirect
 	golang.org/x/net v0.21.0 // indirect
-	golang.org/x/sync v0.6.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240227224415-6ceb2ff114de // indirect
